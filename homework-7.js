0de971e86e2cd3bd77896be42d74dc4a830340d1
ismail <mr.yfam@mail.ru>
2026-03-07 01:12:28 +0300

    fix optimization and formatting

--- homework-7.js
+++ homework-7.js
@@ -1,3 +1,5 @@
+import { socialMediaComments } from "./comments.js";
+
 // (2): Отфильтрованный массив чисел начинающийся с 5.
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
@@ -10,41 +12,39 @@ const availabilityСheckingCar = cars.find(car => car === "Kia");
 
 // (4): Функция переворачивающаяся массив
 function reverseMyArray(myArray) {
-  return myArray.reverse();
+  myArray.reverse();
 };
 
 reverseMyArray(numbers);
 reverseMyArray(cars);
 
 // (7): Вывод в консоль массив комментариев, почта пользователей содержащий в себе ".com".
-import { socialMediaComments } from "./comments.js";
 const containEmailDotCom = socialMediaComments.filter(comment => comment.email.includes(".com"));
 
 // (8): Переборка массива пользователей у которых id <= 5 postId: 2, а те, у кого id > 5, postId: 1.
-const distributedUsers = socialMediaComments.map(comment => {
-  if (comment.id <= 5) {
-    comment.postId = 2;
-    return { ...comment, postId: 2 };
-  } else {
-    return { ...comment, postId: 1 };
-  }
-});
+const distributedUsers = socialMediaComments.map(comment =>
+  comment.id <= 5
+    ? ({ ...comment, postId: 2 })
+    : ({ ...comment, postId: 1 })
+);
 
 // (9): Переборка массива состоящий только из айди и имени.
 const onlyIdName = socialMediaComments.map(({ id, name }) => ({ id, name }));
 
 // (10): Переборка массива с добавлением нового свойства.
-const lengthBodyMessage = socialMediaComments.map(comment => ({ ...comment, isInvalid: comment.body.length > 180 }));
+const lengthBodyMessage = socialMediaComments.map(comment => ({
+  ...comment, isInvalid: comment.body.length > 180
+}));
 
 // (11): Вывод массива из почт с помощью "reduce" и "map".
 const arrayEmailReduce = socialMediaComments.reduce(function (acc, comment) {
-  return [
-    ...acc,
-    comment.email,
-  ]
+  acc.push(comment.email);
+  return acc;
 }, []);
 
-const arrayEmailMap = socialMediaComments.map(comment => ({ [comment.id]: comment.email }));
+const arrayEmailMap = socialMediaComments.map(comment => ({
+  [comment.id]: comment.email
+}));
 
 // (12): Приведение массива с задания №11 к строке, используя методы "toString()" и "join()".
 const arrayEmailJoin = arrayEmailReduce.join(", ");
