5188089823e2b3bcddfd6a82ed9d50bd974233ca
ismail <mr.yfam@mail.ru>
2026-03-22 17:13:26 +0300

    implemented the functions

--- homework-8.js
+++ homework-8.js
@@ -1,35 +1,61 @@
 import { productCards } from "./array-product-cards.js";
 
-
-
 const productCardTemplate = document.getElementById('product-card-template');
 const productCardList = document.getElementById('product-card-list');
 
-productCards.forEach(card => {
-  const cardClone = productCardTemplate.content.cloneNode(true);
+// (5): Функция, которая отрисовывает введённое количество карточек.
+function getAmount() {
+  let cardsToDisplay = Number(prompt('Введите количество карточек от 1 до 5, которое хотите увидеть:'));
+  if (cardsToDisplay >= 1 && cardsToDisplay <= 5) {
+    return cardsToDisplay;
+  } else {
+    alert('Вы ввели неверное число, попробуйте ещё раз!');
+  };
+};
 
-  const img = cardClone.querySelector('.product-card__img');
+const count = getAmount();
 
-  img.src = `images/${card.img}.png`;
-  img.alt = card.productName;
+if (count) {
+  const dataToShow = productCards.slice(0, count);
+  renderCards(dataToShow);
+}
 
-  cardClone.querySelector('.product-card__type-skin').textContent = card.typeSkin;
-  cardClone.querySelector('.product-card-name').textContent = card.productName;
-  cardClone.querySelector('.product-card__description').textContent = card.description;
-  cardClone.querySelector('.product-card__compound').textContent = card.compound.name;
+function renderCards(data) {
+  data.forEach(card => {
+    const cardClone = productCardTemplate.content.cloneNode(true);
 
-  const componentList = cardClone.querySelector('.product-card__active-components-list');
+    const img = cardClone.querySelector('.product-card__img');
 
-  card.compound.activeComponents.forEach(componentText => {
-    const li = document.createElement('li');
-    li.classList.add('product-card__active-components');
-    li.textContent = componentText;
+    img.src = `images/${card.img}.png`;
+    img.alt = card.productName;
 
-    componentList.appendChild(li);
+    cardClone.querySelector('.product-card__type-skin').textContent = card.typeSkin;
+    cardClone.querySelector('.product-card-name').textContent = card.productName;
+    cardClone.querySelector('.product-card__description').textContent = card.description;
+    cardClone.querySelector('.product-card__compound').textContent = card.compound.name;
+
+    const componentList = cardClone.querySelector('.product-card__active-components-list');
+
+    card.compound.activeComponents.forEach(componentText => {
+      const li = document.createElement('li');
+      li.classList.add('product-card__active-components');
+      li.textContent = componentText;
+
+      componentList.appendChild(li);
+    });
+
+    cardClone.querySelector('.product-card__price-name').textContent = card.price.name;
+    cardClone.querySelector('.product-card__price-value').textContent = card.price.value.toLocaleString('ru-RU') + ' ₽';
+    productCardList.appendChild(cardClone);
   });
+};
 
-  cardClone.querySelector('.product-card__price-name').textContent = card.price.name;
-  cardClone.querySelector('.product-card__price-value').textContent = card.price.value.toLocaleString('ru-RU') + ' ₽';
-  productCardList.appendChild(cardClone);
-});
+// (4): Массив объектов с помощью метода reduce. Ключ - название продукта, значение - описание.
+const arrayCard = productCards.reduce((acc, product) => {
+  const newObject = {
+    [product.productName]: product.description
+  };
 
+  acc.push(newObject);
+  return acc
+}, []);
