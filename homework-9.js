1e4f91a2720b209e7320a00c27b699f6bd4b5b27
ismail <mr.yfam@mail.ru>
2026-04-04 15:26:49 +0300

    fix logic and add nav teg

--- homework-9.js
+++ homework-9.js
@@ -15,6 +15,8 @@ const btnRegistration = document.getElementById('registration-button');
 const modalRegistration = document.querySelector('.modal');
 const regForm = document.getElementById('reg-form');
 
+let user;
+
 const hideModal = () => {
   modalRegistration.style.visibility = 'hidden';
   modalRegistration.style.opacity = 0;
@@ -35,6 +37,11 @@ btnRegistration.addEventListener('click', openModal)
 modalRegistration.addEventListener('click', closeModal)
 
 regForm.addEventListener('submit', (event) => {
+  if (!regForm.checkValidity()) {
+    alert('Проверьте введённые данные!')
+    return;
+  };
+
   event.preventDefault();
 
   const formData = new FormData(event.target);
@@ -50,12 +57,14 @@ regForm.addEventListener('submit', (event) => {
 
   delete userData.confirmPassword;
 
-  const user = {
+  user = {
     ...Object.fromEntries(new FormData(event.target)),
     password: passwordValue,
     confirmPassword: confirmPasswordValue,
     createdOn: new Date().toLocaleString()
   };
+
+  Object.freeze(user);
 
   console.log(user);
   alert('Регистрация прошла успешно!');
