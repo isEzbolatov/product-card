c89fc2ae13835e501d598c1e312c19d33b939256
ismail <mr.yfam@mail.ru>
2026-04-10 21:50:53 +0300

    used more form class logic

--- Form.js
+++ Form.js
@@ -4,6 +4,19 @@ export default class Form {
     this.formReset();
   };
 
+  subscribeForm() {
+    const subscribeForm = document.getElementById('email-form');
+
+    subscribeForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const form = event.target;
+      const formData = new FormData(form);
+      const data = Object.fromEntries(formData.entries());
+      console.log(data);
+      subscribeForm.reset();
+    })
+  };
+
   getValues() {
     const formData = new FormData(this.form);
     return Object.fromEntries(formData);
@@ -11,6 +24,19 @@ export default class Form {
 
   formValidity() {
     return this.form.checkValidity();
+  };
+
+  encryptionPassword() {
+    const passwordInput = this.form.querySelector('[name="password"]');
+    const confirmPasswordInput = this.form.querySelector('[name="confirmPassword"]');
+
+    const passwordValue = btoa(passwordInput.value);
+    const confirmPasswordValue = btoa(confirmPasswordInput.value);
+
+    return {
+      password: passwordValue,
+      confirmPassword: confirmPasswordValue,
+    }
   };
 
   formReset() {
--- homework-9.js
+++ homework-9.js
@@ -1,24 +1,16 @@
 import Modal from './Modal.js';
 import Form from './Form.js';
 
-// (4): Валидация кнопки подписаться и вывод в консоль.
-const subscribeForm = document.getElementById('email-form');
+const myRegForm = new Form('reg-form');
 
-subscribeForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const form = event.target;
-  const formData = new FormData(form);
-  const data = Object.fromEntries(formData.entries());
-  console.log(data);
-  subscribeForm.reset();
-})
+// (4): Валидация кнопки подписаться и вывод в консоль.
+myRegForm.subscribeForm();
 
 // (5, 6): Модальной окно регистрации.
 const btnRegistration = document.getElementById('registration-button');
 const regForm = document.getElementById('reg-form');
 
 const myRegWindow = new Modal('modal-registration-id');
-const myRegForm = new Form('reg-form');
 
 btnRegistration.addEventListener('click', () => myRegWindow.open());
 
@@ -28,15 +20,16 @@ regForm.addEventListener('submit', (event) => {
   const isValid = myRegForm.formValidity();
   console.log('Валидность формы:', isValid);
 
-  const formData = new FormData(event.target);
-  let passwordValue = btoa(formData.get('password'));
-  let confirmPasswordValue = btoa(formData.get('confirmPassword'));
+  myRegForm.encryptionPassword();
 
-  if (passwordValue != confirmPasswordValue) {
+  const values = myRegForm.getValues();
+
+  if (values.password !== values.confirmPassword) {
     alert('Пароли не совпадают!');
     return;
   };
 
+  values.password = encrypted.password;
   delete myRegForm.confirmPassword;
 
   Object.freeze(myRegForm);
